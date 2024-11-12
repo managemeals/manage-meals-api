6549f5cacf993c40b4817ef516d60b1ab6383e2a
Hilmar Palsson <hilmar@hilmarp.com>
2024-11-12 20:00:40 +0000

    fix type error

--- src/routes/v1/authed/index.ts
+++ src/routes/v1/authed/index.ts
@@ -14,81 +14,81 @@ import mealPlans from "./meal-plans.js";
 import shoppingLists from "./shopping-lists.js";
 
 const authed = async (fastify: FastifyInstance, options: Object) => {
-  const usersDbCollection = fastify.mongo.client
-    .db(fastify.config.MONGO_DB)
-    .collection<IDbUser>("users");
+    const usersDbCollection = fastify.mongo.client
+        .db(fastify.config.MONGO_DB)
+        .collection<IDbUser>("users");
 
-  const CACHE_PREFIX = "authed_";
+    const CACHE_PREFIX = "authed_";
 
-  fastify.decorateRequest("user", null);
+    fastify.decorateRequest("user");
 
-  fastify.addHook("preHandler", async (request, reply) => {
-    const { authorization } = request.headers;
-    if (!authorization || !authorization.startsWith("Bearer ")) {
-      reply.code(401);
-      throw new Error("Missing Authorization header");
-    }
-
-    const token = authorization.substring(7);
-    let jwtPayload: IJwtUUIDPayload;
-    try {
-      jwtPayload = fastify.jwt.verify<IJwtUUIDPayload>(
-        token,
-        fastify.config.ACCESS_JWT_SECRET
-      );
-    } catch (e) {
-      fastify.log.error(e);
-      reply.code(401);
-      throw new Error("Invalid Authorization header");
-    }
-
-    let user: IDbUser | null;
-    const cacheUser = await fastify.redis.get(
-      `${CACHE_PREFIX}${jwtPayload.uuid}`
-    );
-    if (cacheUser) {
-      user = JSON.parse(cacheUser) as IDbUser;
-    } else {
-      try {
-        user = await usersDbCollection.findOne<IDbUser>({
-          uuid: jwtPayload.uuid,
-        });
-        if (!user) {
-          fastify.log.error(`User ${jwtPayload.uuid} not found`);
-          reply.code(401);
-          throw new Error("Error authorizing");
+    fastify.addHook("preHandler", async (request, reply) => {
+        const { authorization } = request.headers;
+        if (!authorization || !authorization.startsWith("Bearer ")) {
+            reply.code(401);
+            throw new Error("Missing Authorization header");
         }
-      } catch (e) {
-        fastify.log.error(e);
-        throw new Error("Error authorizing");
-      }
-      try {
-        await fastify.redis.set(
-          `${CACHE_PREFIX}${jwtPayload.uuid}`,
-          JSON.stringify(user),
-          "EX",
-          10
+
+        const token = authorization.substring(7);
+        let jwtPayload: IJwtUUIDPayload;
+        try {
+            jwtPayload = fastify.jwt.verify<IJwtUUIDPayload>(
+                token,
+                fastify.config.ACCESS_JWT_SECRET,
+            );
+        } catch (e) {
+            fastify.log.error(e);
+            reply.code(401);
+            throw new Error("Invalid Authorization header");
+        }
+
+        let user: IDbUser | null;
+        const cacheUser = await fastify.redis.get(
+            `${CACHE_PREFIX}${jwtPayload.uuid}`,
         );
-      } catch (e) {
-        fastify.log.error(e);
-      }
-    }
+        if (cacheUser) {
+            user = JSON.parse(cacheUser) as IDbUser;
+        } else {
+            try {
+                user = await usersDbCollection.findOne<IDbUser>({
+                    uuid: jwtPayload.uuid,
+                });
+                if (!user) {
+                    fastify.log.error(`User ${jwtPayload.uuid} not found`);
+                    reply.code(401);
+                    throw new Error("Error authorizing");
+                }
+            } catch (e) {
+                fastify.log.error(e);
+                throw new Error("Error authorizing");
+            }
+            try {
+                await fastify.redis.set(
+                    `${CACHE_PREFIX}${jwtPayload.uuid}`,
+                    JSON.stringify(user),
+                    "EX",
+                    10,
+                );
+            } catch (e) {
+                fastify.log.error(e);
+            }
+        }
 
-    request.user = user;
-  });
+        request.user = user;
+    });
 
-  await fastify.register(admin, { prefix: "/admin" });
-  await fastify.register(settings, { prefix: "/settings" });
-  await fastify.register(tags, { prefix: "/tags" });
-  await fastify.register(categories, { prefix: "/categories" });
-  await fastify.register(recipes, { prefix: "/recipes" });
-  await fastify.register(search, { prefix: "/search" });
-  await fastify.register(help, { prefix: "/help" });
-  await fastify.register(subscriptions, { prefix: "/subscriptions" });
-  await fastify.register(premium, { prefix: "/" });
-  await fastify.register(share, { prefix: "/share" });
-  await fastify.register(mealPlans, { prefix: "/meal-plans" });
-  await fastify.register(shoppingLists, { prefix: "/shopping-lists" });
+    await fastify.register(admin, { prefix: "/admin" });
+    await fastify.register(settings, { prefix: "/settings" });
+    await fastify.register(tags, { prefix: "/tags" });
+    await fastify.register(categories, { prefix: "/categories" });
+    await fastify.register(recipes, { prefix: "/recipes" });
+    await fastify.register(search, { prefix: "/search" });
+    await fastify.register(help, { prefix: "/help" });
+    await fastify.register(subscriptions, { prefix: "/subscriptions" });
+    await fastify.register(premium, { prefix: "/" });
+    await fastify.register(share, { prefix: "/share" });
+    await fastify.register(mealPlans, { prefix: "/meal-plans" });
+    await fastify.register(shoppingLists, { prefix: "/shopping-lists" });
 };
 
 export default authed;
