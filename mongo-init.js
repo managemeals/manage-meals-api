62876ff13b9f3e15721f4951a5debbbd87aa8a5e
Hilmar Palsson <hilmar@hilmarp.com>
2026-03-03 17:21:09 +0000

    add indexes

--- mongo-init.js
+++ mongo-init.js
@@ -1,30 +1,49 @@
-db.createCollection("users");
+try {
+  db.createCollection("users");
+} catch {}
 db.users.createIndex({ uuid: 1 }, { unique: true });
 db.users.createIndex({ email: 1 }, { unique: true });
 
-db.createCollection("recipes");
+try {
+  db.createCollection("recipes");
+} catch {}
 db.recipes.createIndex({ uuid: 1 }, { unique: true });
 db.recipes.createIndex({ slug: 1 }, { unique: true });
 db.recipes.createIndex({ createdByUuid: 1, slug: 1 }, { unique: true });
+db.recipes.createIndex({ createdByUuid: 1, tagUuids: 1 });
+db.recipes.createIndex({ createdByUuid: 1, categoryUuids: 1 });
+db.recipes.createIndex({ createdByUuid: 1, createdAt: -1 });
+db.recipes.createIndex({ "data.canonical_url": 1 }, { sparse: true });
 
-db.createCollection("tags");
+try {
+  db.createCollection("tags");
+} catch {}
 db.tags.createIndex({ uuid: 1 }, { unique: true });
 db.tags.createIndex({ createdByUuid: 1, slug: 1 }, { unique: true });
 
-db.createCollection("categories");
+try {
+  db.createCollection("categories");
+} catch {}
 db.categories.createIndex({ uuid: 1 }, { unique: true });
 db.categories.createIndex({ createdByUuid: 1, slug: 1 }, { unique: true });
 
-db.createCollection("mealplans");
+try {
+  db.createCollection("mealplans");
+} catch {}
 db.mealplans.createIndex({ uuid: 1 }, { unique: true });
 db.mealplans.createIndex({ uuid: 1, createdByUuid: 1 });
 db.mealplans.createIndex({ date: 1, createdByUuid: 1 });
+db.mealplans.createIndex({ createdByUuid: 1, updatedAt: -1 });
 
-db.createCollection("shoppinglists");
+try {
+  db.createCollection("shoppinglists");
+} catch {}
 db.shoppinglists.createIndex({ uuid: 1 }, { unique: true });
 db.shoppinglists.createIndex({ slug: 1, createdByUuid: 1 }, { unique: true });
 
-db.createCollection("sharerecipes");
+try {
+  db.createCollection("sharerecipes");
+} catch {}
 db.sharerecipes.createIndex({ uuid: 1 }, { unique: true });
 db.sharerecipes.createIndex({ slug: 1 }, { unique: true });
 db.sharerecipes.createIndex({ slug: 1, createdByUuid: 1 });
